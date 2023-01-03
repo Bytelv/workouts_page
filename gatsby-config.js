35944f1532a063610a2d8adfa8b8023b72117535
字节君 <96653078+Bytelv@users.noreply.github.com>
2023-01-03 18:05:52 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,17 @@
 module.exports = {
   pathPrefix: '/workouts_page', // Change to `/workouts_page` when running on GitHub pages
   siteMetadata: {
-    siteTitle: 'Workouts Map',
-    siteUrl: 'https://ben29.xyz',
-    logo: 'https://avatars3.githubusercontent.com/u/6956444?s=460&u=97e1062227c4088db3c987146455245c6fa78441',
-    description: 'Personal Workouts Page',
+    siteTitle: '字节君の运动网站',
+    siteUrl: 'https://sport.lvbyte.tk',
+    logo: 'hhttps://c2.im5i.com/2022/12/30/Y18D2.jpeg',
+    description: '个人运动网站',
     keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
     navLinks: [
       {
         name: 'Blog',
-        url: 'https://ben29.xyz',
+        url: 'https://space.lvbyte.tk',
       },
-      {
-        name: 'About',
-        url: 'https://github.com/ben-29/workouts_page/blob/master/README-CN.md',
-      },
+      
     ],
   },
   plugins: [
